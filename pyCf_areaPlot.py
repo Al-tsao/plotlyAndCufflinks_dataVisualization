de5d4640fd52267c3227d100f9f3a9258ef6f884
Al-tsao <al.tsao.yw@gmail.com>
2021-12-16 11:10:33 +0800

    revise

---
+++ pyCf_areaPlot.py
@@ -0,0 +1,22 @@
+#載入模組
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from plotly.offline import iplot
+import plotly as py
+import cufflinks as cf
+import plotly.graph_objects as go
+import plotly.express as px
+
+#載入資料
+df = pd.DataFrame(np.random.randn(100, 3), columns = ['A', 'B', 'C'])
+df = df.stack().reset_index().iloc[:, ::-1]
+df.columns = ['Value', 'Column Name', 'Index']
+df['Value'] = df['Value'].cumsum()
+print(df)
+
+#繪圖
+fig = px.area(df, x="Index", y="Value", color='Column Name')
+fig.show()
+
